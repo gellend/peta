76f292d309cb9db325d0bc3809cb45319c32f9cd
gellenn <hellogellen@gmail.com>
2022-10-08 21:59:09 +0700

    implement register function

---
+++ context/userContext.js
@@ -0,0 +1,51 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+import { createFirebaseApp } from '../firebase/clientApp'
+import { useRouter } from 'next/router'
+
+export const UserContext = createContext()
+
+export default function UserContextComp({ children }) {
+  const router = useRouter()
+  const [user, setUser] = useState(null)
+  const [loadingUser, setLoadingUser] = useState(true) // Helpful, to update the UI accordingly.
+
+  useEffect(() => {
+    // Listen authenticated user
+    const app = createFirebaseApp()
+    const auth = getAuth(app)
+    const unsubscriber = onAuthStateChanged(auth, async (user) => {
+      try {
+        if (user) {
+          // User is signed in.
+          const { uid, displayName, email, photoURL } = user
+          // You could also look for the user doc in your Firestore (if you have one):
+          // const userDoc = await firebase.firestore().doc(`users/${uid}`).get()
+          setUser({ uid, displayName, email, photoURL })
+
+          router.push('/')
+        } else {
+          console.log('there is no user logged in')
+          setUser(null)
+        }
+      } catch (error) {
+        // Most probably a connection error. Handle appropriately.
+      } finally {
+        setLoadingUser(false)
+      }
+    })
+
+    // Unsubscribe auth listener on unmount
+    return () => unsubscriber()
+  }, [])
+
+  return (
+    <UserContext.Provider value={{ user, setUser, loadingUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+// Custom hook that shorthands the context!
+export const useUser = () => useContext(UserContext)
--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -11,80 +11,89 @@ import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-
-const theme = createTheme();
+import { createFirebaseApp } from '../firebase/clientApp'
+import { useRouter } from 'next/router';
 
 export default function Register() {
+  const router = useRouter()
+  const app = createFirebaseApp()
+  const auth = getAuth(app)
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+
+    createUserWithEmailAndPassword(auth, data.get('email'), data.get('password'))
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+
+        router.push('/')
+      })
+      .catch((error) => {
+        console.log(error.code);
+      });
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Registrasi Akun
-          </Typography>
-          <Typography component="h1" variant="h5">
-            PETA - Pengajuan Judul TA
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Alamat Email"
-              name="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Kata Sandi"
-              type="password"
-              id="password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Register
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Lupa kata sandi?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Sudah punya akun
-                </Link>
-              </Grid>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Registrasi Akun
+        </Typography>
+        <Typography component="h1" variant="h5">
+          PETA - Pengajuan Judul TA
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Alamat Email"
+            name="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Kata Sandi"
+            type="password"
+            id="password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Register
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Lupa kata sandi?
+              </Link>
             </Grid>
-          </Box>
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Sudah punya akun
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 }
