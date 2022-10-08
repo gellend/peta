1fb233a728de9822eee18aa28112109ffb088b82
gellenn <hellogellen@gmail.com>
2022-10-08 22:13:24 +0700

    implement login function

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -13,84 +13,94 @@ import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-
-const theme = createTheme();
+import { createFirebaseApp } from '../firebase/clientApp'
+import { useRouter } from 'next/router';
 
 export default function LogIn() {
+  const router = useRouter()
+  const app = createFirebaseApp()
+  const auth = getAuth(app)
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+
+    signInWithEmailAndPassword(auth, data.get('email'), data.get('password'))
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+
+        router.push('/')
+      })
+      .catch((error) => {
+        console.log(error.code);
+      });
+
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h5">
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Masuk
+        </Typography>
+        <Typography component="h1" variant="h5">
+          PETA - Pengajuan Judul TA
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Alamat Email"
+            name="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Kata Sandi"
+            type="password"
+            id="password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Ingat saya"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
             Masuk
-          </Typography>
-          <Typography component="h1" variant="h5">
-            PETA - Pengajuan Judul TA
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Alamat Email"
-              name="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Kata Sandi"
-              type="password"
-              id="password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Ingat saya"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Masuk
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Lupa kata sandi?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  Buat akun
-                </Link>
-              </Grid>
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Lupa kata sandi?
+              </Link>
             </Grid>
-          </Box>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                Buat akun
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
   );
 }
