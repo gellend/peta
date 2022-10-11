c4b5e1e21825f12dcfd2fca9dde3bc681ef80053
gellenn <hellogellen@gmail.com>
2022-10-11 21:39:30 +0700

    update dashboard page

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,9 +1,76 @@
 import * as React from 'react';
 
-import Dashboard from '../../src/Dashboard';
+import { Box, Container, CssBaseline, Grid, Paper, Toolbar, Typography } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
-export default function Index() {
+import Navbar from '../../src/components/Navbar';
+
+const mdTheme = createTheme();
+
+export default function Dashboard() {
   return (
-    <Dashboard></Dashboard>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navbar />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={4}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 140,
+                  }}
+                >
+                  <Typography variant="h5" sx={{ mb: 2 }}>Total Pengajuan Judul TA</Typography>
+                  <Typography variant="h5">18</Typography>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 140,
+                  }}
+                >
+                  <Typography variant="h5" sx={{ mb: 2 }}>Pengajuan Judul TA Disetujui</Typography>
+                  <Typography variant="h5">9</Typography>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} md={4}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 140,
+                  }}
+                >
+                  <Typography variant="h5" sx={{ mb: 2 }}>Pengajuan Judul TA Ditolak</Typography>
+                  <Typography variant="h5">9</Typography>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
