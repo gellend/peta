983c8678290937666453063c494c55ff04e50a4b
gellenn <hellogellen@gmail.com>
2022-12-21 21:26:37 +0700

    update profil page to check if user already logged in

--- pages/profil/index.js
+++ pages/profil/index.js
@@ -19,87 +19,115 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Edit } from "@mui/icons-material";
 import Link from "next/link";
 import Navbar from "../../src/components/Navbar";
-import { useUser } from "../../context/userContext";
+import { createFirebaseApp } from "../../firebase/clientApp";
+import { getAuth } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { useRouter } from "next/router";
 
 const mdTheme = createTheme();
 
 export default function Profil() {
-  const { user, setUser, loadingUser } = useUser();
+  const app = createFirebaseApp();
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const router = useRouter();
+
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const userData = await getDoc(doc(db, "users", user.uid));
+        setUser(userData.data());
+        setIsLoading(false);
+      } else {
+        router.push("/");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <Navbar />
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              <Grid item xs={12} md={6}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <Stack
-                    direction="row"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    sx={{ mb: 3 }}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          <Navbar />
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === "light"
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: "100vh",
+              overflow: "auto",
+            }}
+          >
+            <Toolbar />
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} md={6}>
+                  <Paper
+                    sx={{
+                      p: 2,
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
                   >
-                    <Typography variant="h5">
-                      Profil {user ? user.nama : ""}
-                    </Typography>
-                    <Link href="/profil/update">
-                      <IconButton>
-                        <Edit />
-                      </IconButton>
-                    </Link>
-                  </Stack>
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="/static/images/avatar/1.jpg"
-                    sx={{ width: 128, height: 128, mb: 3 }}
-                  />
-                  <Table aria-label="Data Diri">
-                    <TableBody>
-                      <TableRow>
-                        <TableCell>NRP</TableCell>
-                        <TableCell>{user ? user.nrp : ""}</TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell>Nama</TableCell>
-                        <TableCell>{user ? user.nama : ""}</TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell>Email</TableCell>
-                        <TableCell>{user ? user.email : ""}</TableCell>
-                      </TableRow>
-                      <TableRow>
-                        <TableCell>Role</TableCell>
-                        <TableCell>{user ? user.role : ""}</TableCell>
-                      </TableRow>
-                    </TableBody>
-                  </Table>
-                </Paper>
+                    <Stack
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                      sx={{ mb: 3 }}
+                    >
+                      <Typography variant="h5">
+                        Profil {user ? user.nama : ""}
+                      </Typography>
+                      <Link href="/profil/update">
+                        <IconButton>
+                          <Edit />
+                        </IconButton>
+                      </Link>
+                    </Stack>
+                    <Avatar
+                      alt={user ? user.nama : ""}
+                      src="/static/images/avatar/1.jpg"
+                      sx={{ width: 128, height: 128, mb: 3 }}
+                    />
+                    <Table aria-label="Data Diri">
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>NRP</TableCell>
+                          <TableCell>{user ? user.nrp : ""}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Nama</TableCell>
+                          <TableCell>{user ? user.nama : ""}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Email</TableCell>
+                          <TableCell>{user ? user.email : ""}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell>Role</TableCell>
+                          <TableCell>{user ? user.role : ""}</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </Paper>
+                </Grid>
               </Grid>
-            </Grid>
-          </Container>
+            </Container>
+          </Box>
         </Box>
-      </Box>
+      )}
     </ThemeProvider>
   );
 }
