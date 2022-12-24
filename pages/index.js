f864fa9417d7f50527b28e40608e4fd3bbf41775
gellenn <hellogellen@gmail.com>
2022-12-24 11:58:24 +0700

    add check if user already logged in

--- pages/index.js
+++ pages/index.js
@@ -26,9 +26,26 @@ export default function LogIn() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggingIn, setIsLoggingIn] = useState(true);
 
   const { user, setUser, loadingUser, setLoadingUser } = useUser();
 
+  // Check if user is already logged in
+  useEffect(() => {
+    setIsLoggingIn(true);
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const userData = await getDoc(doc(db, "users", user.uid));
+        setUser(userData.data());
+      }
+
+      setIsLoggingIn(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  // Redirect user to dashboard if they are already logged in
   useEffect(() => {
     if (user && user.role === "Mahasiswa") {
       router.push("/pengajuan");
@@ -40,18 +57,16 @@ export default function LogIn() {
     }
   }, [user, loadingUser]);
 
-  const getUserData = async (uid) => {
-    const user = await getDoc(doc(db, "users", uid));
-    return user.data();
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoadingUser(true);
 
     signInWithEmailAndPassword(auth, email, password)
       .then(async (userCredential) => {
-        setUser(await getUserData(userCredential.user.uid));
+        const userData = await getDoc(
+          doc(db, "users", userCredential.user.uid)
+        );
+        setUser(userData.data());
       })
       .catch((error) => {
         console.log(error.code, error.message);
@@ -65,63 +80,70 @@ export default function LogIn() {
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Masuk
-        </Typography>
-        <Typography component="h1" variant="h5">
-          PETA - Pengajuan Judul TA
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Alamat Email"
-            autoFocus
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            label="Kata Sandi"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            disabled={loadingUser}
+
+      {!isLoggingIn ? (
+        <>
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            {loadingUser ? "Loading..." : "Masuk"}
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Lupa kata sandi?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/register" variant="body2">
-                Buat akun
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
+            <Typography component="h1" variant="h5">
+              Masuk
+            </Typography>
+            <Typography component="h1" variant="h5">
+              PETA - Pengajuan Judul TA
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Alamat Email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Kata Sandi"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={loadingUser}
+              >
+                {loadingUser ? "Loading..." : "Masuk"}
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Lupa kata sandi?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    Buat akun
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </Container>
   );
 }
