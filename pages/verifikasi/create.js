a42e42a375568a548bb63d96e2ef9f7f91a6227f
gellend <hellogellen@gmail.com>
2023-06-26 19:38:15 +0700

    chore: implement custom snackbar

--- pages/verifikasi/create.js
+++ pages/verifikasi/create.js
@@ -10,28 +10,23 @@ import {
   TableRow,
   Toolbar,
   Button,
-  TableHead,
   Stack,
   IconButton,
   Typography,
   TextField,
-  MenuItem,
-  Snackbar,
-  Alert
+  MenuItem
 } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { ArrowBack, Edit } from "@mui/icons-material";
+import { ArrowBack } from "@mui/icons-material";
 
-import Link from "next/link";
 import Navbar from "../../src/components/Navbar";
+import CustomSnackbar from "../../src/components/CustomSnackbar";
 import { createFirebaseApp } from "../../firebase/clientApp";
 import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
 import {
-  collection,
   doc,
   getDoc,
-  getDocs,
   getFirestore,
   setDoc,
   serverTimestamp
@@ -58,9 +53,7 @@ export default function CreateUser() {
   const [role, setRole] = useState("Mahasiswa");
 
   // Snackbar
-  const [openSnackBar, setOpenSnackBar] = useState(false);
-  const [messageSnackBar, setMessageSnackBar] = useState("");
-  const [typeSnackBar, setTypeSnackBar] = useState("");
+  const [snackbarData, setSnackbarData] = useState({ open: false, message: "", type: "" });
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(async (user) => {
@@ -75,14 +68,6 @@ export default function CreateUser() {
 
     return unsubscribe;
   }, []);
-
-  const handleCloseSnackBar = () => setOpenSnackBar(false);
-
-  const handleOpenSnackBar = (message, type) => {
-    setMessageSnackBar(message);
-    setTypeSnackBar(type);
-    setOpenSnackBar(true);
-  }
 
   const resetForm = () => {
     setId("")
@@ -117,140 +102,153 @@ export default function CreateUser() {
       });
   };
 
+  // Handle snackbar
+  const handleOpenSnackBar = (message, type) => {
+    setSnackbarData({ open: true, message, type });
+  };
+
+  const handleCloseSnackBar = () => {
+    setSnackbarData({ ...snackbarData, open: false });
+  };
+
   return (
     <ThemeProvider theme={mdTheme}>
       {isLoggingIn ? (
         <div>Loading...</div>
       ) : (
-        <Box sx={{ display: "flex" }}>
-          <CssBaseline />
-          <Navbar />
-          <Box
-            component="main"
-            sx={{
-              backgroundColor: (theme) =>
-                theme.palette.mode === "light"
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
-              flexGrow: 1,
-              height: "100vh",
-              overflow: "auto",
-            }}
-          >
-            <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-              <Stack
-                direction="row"
-                spacing={2}
-                alignItems="center"
-                // justifyContent="space-between"
-                sx={{ mb: 3 }}
-              >
-                <Stack direction="row" spacing={2} alignItems="center">
-                  <IconButton onClick={() => router.back()}>
-                    <ArrowBack />
-                  </IconButton>
-                  <Typography variant="h5">Tambah Pengguna</Typography>
+        <>
+          <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <Navbar />
+            <Box
+              component="main"
+              sx={{
+                backgroundColor: (theme) =>
+                  theme.palette.mode === "light"
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[900],
+                flexGrow: 1,
+                height: "100vh",
+                overflow: "auto",
+              }}
+            >
+              <Toolbar />
+              <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                <Stack
+                  direction="row"
+                  spacing={2}
+                  alignItems="center"
+                  // justifyContent="space-between"
+                  sx={{ mb: 3 }}
+                >
+                  <Stack direction="row" spacing={2} alignItems="center">
+                    <IconButton onClick={() => router.back()}>
+                      <ArrowBack />
+                    </IconButton>
+                    <Typography variant="h5">Tambah Pengguna</Typography>
+                  </Stack>
                 </Stack>
-              </Stack>
-              <Paper
-                sx={{
-                  p: 2,
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <Grid container spacing={3}>
-                  <Grid item xs={12} lg={5}>
-                    <Table aria-label="Tambah-Pengguna">
-                      <TableBody>
-                        <TableRow>
-                          <TableCell>
-                            <TextField
-                              label={role === "Mahasiswa" ? "NRP" : "NIDN"}
-                              variant="outlined"
-                              fullWidth
-                              value={id}
-                              onChange={(e) => setId(e.target.value)}
-                            />
-                          </TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <TextField
-                              label="Nama Lengkap"
-                              variant="outlined"
-                              fullWidth
-                              value={nama}
-                              onChange={(e) => setNama(e.target.value)}
-                            />
-                          </TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <TextField
-                              label="Alamat Email"
-                              variant="outlined"
-                              fullWidth
-                              value={email}
-                              onChange={(e) => setEmail(e.target.value)}
-                            />
-                          </TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <TextField
-                              select
-                              label="Role"
-                              fullWidth
-                              value={role}
-                              onChange={(e) => setRole(e.target.value)}
-                            >
-                              <MenuItem value="Mahasiswa">Mahasiswa</MenuItem>
-                              <MenuItem value="Dosen">Dosen</MenuItem>
-                            </TextField>
-                          </TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <TextField
-                              label="Kata Sandi"
-                              variant="outlined"
-                              fullWidth
-                              type="password"
-                              value={password}
-                              onChange={(e) => setPassword(e.target.value)}
-                            />
-                          </TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell sx={{ borderBottom: "none" }}>
-                            <Box display="flex" justifyContent="center">
-                              <Button
-                                sx={{ minWidth: 200 }}
-                                variant="contained"
-                                color="success"
-                                type="submit"
-                                onClick={handleSubmit}
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} lg={5}>
+                      <Table aria-label="Tambah-Pengguna">
+                        <TableBody>
+                          <TableRow>
+                            <TableCell>
+                              <TextField
+                                label={role === "Mahasiswa" ? "NRP" : "NIDN"}
+                                variant="outlined"
+                                fullWidth
+                                value={id}
+                                onChange={(e) => setId(e.target.value)}
+                              />
+                            </TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>
+                              <TextField
+                                label="Nama Lengkap"
+                                variant="outlined"
+                                fullWidth
+                                value={nama}
+                                onChange={(e) => setNama(e.target.value)}
+                              />
+                            </TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>
+                              <TextField
+                                label="Alamat Email"
+                                variant="outlined"
+                                fullWidth
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                              />
+                            </TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>
+                              <TextField
+                                select
+                                label="Role"
+                                fullWidth
+                                value={role}
+                                onChange={(e) => setRole(e.target.value)}
                               >
-                                Submit
-                              </Button>
-                            </Box>
-                          </TableCell>
-                        </TableRow>
-                      </TableBody>
-                    </Table>
+                                <MenuItem value="Mahasiswa">Mahasiswa</MenuItem>
+                                <MenuItem value="Dosen">Dosen</MenuItem>
+                              </TextField>
+                            </TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell>
+                              <TextField
+                                label="Kata Sandi"
+                                variant="outlined"
+                                fullWidth
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                              />
+                            </TableCell>
+                          </TableRow>
+                          <TableRow>
+                            <TableCell sx={{ borderBottom: "none" }}>
+                              <Box display="flex" justifyContent="center">
+                                <Button
+                                  sx={{ minWidth: 200 }}
+                                  variant="contained"
+                                  color="success"
+                                  type="submit"
+                                  onClick={handleSubmit}
+                                >
+                                  Submit
+                                </Button>
+                              </Box>
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </Grid>
                   </Grid>
-                </Grid>
-              </Paper>
-            </Container>
+                </Paper>
+              </Container>
+            </Box>
           </Box>
-          <Snackbar open={openSnackBar} autoHideDuration={2000} onClose={handleCloseSnackBar}>
-            <Alert onClose={handleCloseSnackBar} severity={typeSnackBar} sx={{ width: '100%' }}>
-              {messageSnackBar}
-            </Alert>
-          </Snackbar>
-        </Box>
+
+          <CustomSnackbar
+            open={snackbarData.open}
+            message={snackbarData.message}
+            type={snackbarData.type}
+            onClose={handleCloseSnackBar}
+          />
+        </>
       )}
     </ThemeProvider>
   );
