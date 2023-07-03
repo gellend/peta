35371ed67fde117685eb8ae1e7343a9442e0a04b
gellend <hellogellen@gmail.com>
2023-07-03 10:41:38 +0700

    chore: refactoring customsnackbar

--- pages/test.js
+++ pages/test.js
@@ -1,12 +1,35 @@
+import CustomSnackbar from "../src/components/CustomSnackbar";
 import useAppStore from "../src/store/global";
 
 export default function Test() {
-  const { isLoading, setIsLoading } = useAppStore((state) => state);
+  const {
+    isLoading,
+    setIsLoading,
+    snackbarData,
+    handleOpenSnackBar,
+    handleCloseSnackBar,
+  } = useAppStore((state) => state);
 
   return (
-    <div>
-      <h1>is Loading? {isLoading.toString()}</h1>
-      <button onClick={() => setIsLoading(!isLoading)}>Klik</button>
-    </div>
+    <>
+      <div>
+        <h1>is Loading? {isLoading.toString()}</h1>
+        <button onClick={() => setIsLoading(!isLoading)}>Klik</button>
+        <br />
+        <button onClick={() => handleOpenSnackBar("Halo", "success")}>
+          Open CustomSnackbar With Type
+        </button>
+        <br />
+        <button onClick={() => handleOpenSnackBar("Halo")}>
+          Open CustomSnackbar Without Type
+        </button>
+      </div>
+      <CustomSnackbar
+        open={snackbarData.open}
+        message={snackbarData.message}
+        type={snackbarData.type}
+        onClose={() => handleCloseSnackBar()}
+      />
+    </>
   );
 }
--- src/components/CustomSnackbar.js
+++ src/components/CustomSnackbar.js
@@ -1,17 +1,27 @@
 import { Snackbar, Alert } from "@mui/material";
 
 const CustomSnackbar = ({ open, message, type, onClose }) => {
-  return (
-    <Snackbar open={open} autoHideDuration={2000} onClose={onClose}>
-      {type ? (
+  if (!open) {
+    return null;
+  }
+
+  const snackbarProps = {
+    open: true,
+    autoHideDuration: 2000,
+    onClose: onClose,
+  };
+
+  if (type) {
+    return (
+      <Snackbar {...snackbarProps}>
         <Alert onClose={onClose} severity={type} sx={{ width: "100%" }}>
           {message}
         </Alert>
-      ) : (
-        <>{message}</>
-      )}
-    </Snackbar>
-  );
+      </Snackbar>
+    );
+  } else {
+    return <Snackbar {...snackbarProps} message={message} />;
+  }
 };
 
 export default CustomSnackbar;
