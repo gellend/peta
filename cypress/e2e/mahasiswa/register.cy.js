f79e14cd681fce6720fc25791707b2604938bdc4
gellend <hellogellen@gmail.com>
2023-09-18 20:14:08 +0700

    feat: add prodi when register

--- cypress/e2e/mahasiswa/register.cy.js
+++ cypress/e2e/mahasiswa/register.cy.js
@@ -4,51 +4,70 @@ describe("Register", () => {
   });
 
   it("Menampilkan error jika NRP kosong", () => {
+    cy.getByData("register-prodi").click();
+    cy.getByData("register-prodi-option-0").click();
     cy.getByData("register-submit").click();
     cy.getByData("register-id-helper-text").should(
       "contain.text",
-      "NRP is required"
+      "NRP tidak boleh kosong"
+    );
+  });
+
+  it("Menampilkan error jika program studi kosong", () => {
+    cy.getByData("register-id").type("12345");
+    cy.getByData("register-submit").click();
+    cy.getByData("register-prodi-helper-text").should(
+      "contain.text",
+      "Program Studi tidak boleh kosong"
     );
   });
 
   it("Menampilkan error jika Nama Lengkap kosong", () => {
     cy.getByData("register-id").type("12345");
+    cy.getByData("register-prodi").click();
+    cy.getByData("register-prodi-option-0").click();
     cy.getByData("register-submit").click();
     cy.getByData("register-nama-helper-text").should(
       "contain.text",
-      "Nama Lengkap is required"
+      "Nama Lengkap tidak boleh kosong"
     );
   });
 
   it("Menampilkan error jika Alamat Email kosong", () => {
     cy.getByData("register-id").type("12345");
+    cy.getByData("register-prodi").click();
+    cy.getByData("register-prodi-option-0").click();
     cy.getByData("register-nama").type("John Doe");
     cy.getByData("register-submit").click();
     cy.getByData("register-email-helper-text").should(
       "contain.text",
-      "Alamat Email is required"
+      "Alamat email tidak boleh kosong"
     );
   });
 
   it("Menampilkan error jika Alamat Email tidak valid", () => {
     cy.getByData("register-id").type("12345");
+    cy.getByData("register-prodi").click();
+    cy.getByData("register-prodi-option-0").click();
     cy.getByData("register-nama").type("John Doe");
     cy.getByData("register-email").type("invalid-email");
     cy.getByData("register-submit").click();
     cy.getByData("register-email-helper-text").should(
       "contain.text",
-      "Invalid email format"
+      "Alamat email tidak valid"
     );
   });
 
   it("Menampilkan error jika Kata Sandi kosong", () => {
     cy.getByData("register-id").type("12345");
+    cy.getByData("register-prodi").click();
+    cy.getByData("register-prodi-option-0").click();
     cy.getByData("register-nama").type("John Doe");
     cy.getByData("register-email").type("john.doe@example.com");
     cy.getByData("register-submit").click();
     cy.getByData("register-password-helper-text").should(
       "contain.text",
-      "Kata Sandi is required"
+      "Kata Sandi tidak boleh kosong"
     );
   });
 
@@ -56,6 +75,8 @@ describe("Register", () => {
     cy.fixture("mahasiswa/user.json").as("userMahasiswa");
     cy.get("@userMahasiswa").then((user) => {
       cy.getByData("register-id").type(user.id);
+      cy.getByData("register-prodi").click();
+      cy.getByData("register-prodi-option-0").click();
       cy.getByData("register-id-input").should("have.value", user.id);
       cy.getByData("register-nama").type(user.nama);
       cy.getByData("register-nama-input").should("have.value", user.nama);
--- pages/register.js
+++ pages/register.js
@@ -6,6 +6,7 @@ import {
   Link,
   TextField,
   Typography,
+  MenuItem,
 } from "@mui/material";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { serverTimestamp } from "firebase/firestore";
@@ -18,27 +19,30 @@ import useAppStore from "../src/store/global";
 import { postData } from "../src/lib/store";
 import { auth, getCurrentLoginUser } from "../src/lib/auth";
 import { useEffect } from "react";
+import config from "../src/const/config.json";
 
 export default function Register() {
   const router = useRouter();
 
   const initialState = {
     id: "",
+    prodi: "",
     nama: "",
     email: "",
     password: "",
   };
 
   const validationRules = {
-    id: (value) => (!value ? "NRP is required" : ""),
-    nama: (value) => (!value ? "Nama Lengkap is required" : ""),
+    id: (value) => (!value ? "NRP tidak boleh kosong" : ""),
+    prodi: (value) => (!value ? "Program Studi tidak boleh kosong" : ""),
+    nama: (value) => (!value ? "Nama Lengkap tidak boleh kosong" : ""),
     email: (value) =>
       !value
-        ? "Alamat Email is required"
+        ? "Alamat email tidak boleh kosong"
         : !isValidEmail(value)
-        ? "Invalid email format"
+        ? "Alamat email tidak valid"
         : "",
-    password: (value) => (!value ? "Kata Sandi is required" : ""),
+    password: (value) => (!value ? "Kata Sandi tidak boleh kosong" : ""),
   };
 
   const { values, errors, handleChange, validateForm } = useForm(
@@ -63,6 +67,7 @@ export default function Register() {
 
         const dataToStore = {
           id: values.id,
+          prodi: values.prodi,
           nama: values.nama,
           email: values.email,
           role: "Mahasiswa",
@@ -71,7 +76,10 @@ export default function Register() {
 
         const success = await postData("users", dataToStore, res.user.uid);
 
-        if (success) router.push("/");
+        if (success) {
+          handleOpenSnackBar("Registrasi berhasil", "success");
+          router.push("/");
+        }
       } catch (error) {
         handleOpenSnackBar(error.message, "error");
       }
@@ -113,6 +121,31 @@ export default function Register() {
             error={!!errors.id}
             helperText={errors.id}
           />
+          <TextField
+            data-cy="register-prodi"
+            inputProps={{ "data-cy": "register-prodi-input" }}
+            FormHelperTextProps={{ "data-cy": "register-prodi-helper-text" }}
+            select
+            margin="normal"
+            fullWidth
+            label="Program Studi"
+            name="prodi"
+            value={values.prodi}
+            onChange={handleChange}
+            error={!!errors.prodi}
+            helperText={errors.prodi}
+          >
+            {config.prodi &&
+              config.prodi.map((option, i) => (
+                <MenuItem
+                  key={option}
+                  value={option}
+                  data-cy={`register-prodi-option-${i}`}
+                >
+                  {option}
+                </MenuItem>
+              ))}
+          </TextField>
           <TextField
             data-cy="register-nama"
             inputProps={{ "data-cy": "register-nama-input" }}
